a8ae987369c7fa4afca0ea304b8e83c81b4fa59f
Matthew Greco <mgreco2112@tutanota.com>
2025-02-26 13:26:38 -0500

    started CSS work on Displaylanders, created nav buttons to headers

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -5,8 +5,8 @@ const Button = (props) => {
 
   return (
     <button
+      id={props.id}
       type={props.type || "submit"}
-      style={{...styles.button, ...props.style}}
       onClick={props.onClick}
       disabled={props.disabled}
     >
@@ -20,22 +20,6 @@ const Button = (props) => {
 
 }
 
-const styles = {
-  button: {
-    alignSelf: 'center',
-    backgroundColor: '#3343f2',
-    marginLeft: 5,
-    marginRight: 5,
-    marginBottom: 10,
-    marginTop: 5,
-    width: "50%",
-    borderRadius: 10,
-    fontSize: 16,
-    paddingTop: 5,
-    paddingBottom: 5,
-    fontWeight: "bold",
-    fontFamily: "sans-serif"
-  }
-}
+
 
 export default Button;
--- src/components/landers/DisplayLander.js
+++ src/components/landers/DisplayLander.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Container from "../common/Container";
+import Button from "../common/Button";
+import "./Landers.css";
 import { useParams } from "react-router-dom";
 import { apiHostURL } from "../../config";
+import { useNavigate } from "react-router-dom";
 
 const DisplayLander = () => {
     const params = useParams();
+    const navigate = useNavigate();
 
     const [lander, setLander] = useState({
         asdblanderID: params.id
@@ -29,6 +33,18 @@ const DisplayLander = () => {
         _fetchLander();
     }, [lander.asdblanderID]);
 
+    const ctdNav = () => {
+        navigate(`/ctdhead/${lander.ctdheads[0].headID}`);
+    }
+
+    const doNav = () => {
+        navigate(`/dohead/${lander.doheads[0].headID}`);
+    }
+
+    const flntuNav = () => {
+        navigate(`/flntuhead/${lander.flntuheads[0].headID}`);
+    }
+
 
 
 
@@ -43,6 +59,38 @@ const DisplayLander = () => {
                         <p>CTD Header ID: {lander.ctdheads[0].headID}</p>
                         <p>DO Header ID: {lander.doheads[0].headID}</p>
                         <p>FLNTU Header ID: {lander.flntuheads[0].headID}</p>
+                        <div id="ButtonDiv">
+                            {
+                                lander.ctdheads.length !== 0
+                                ?
+                                <Button 
+                                    id="LanderButton"
+                                    onClick={ctdNav}
+                                >CTD Info</Button>
+                                :
+                                null
+                            }
+                                                        {
+                                lander.ctdheads.length !== 0
+                                ?
+                                <Button 
+                                    id="LanderButton"
+                                    onClick={doNav}
+                                >DO Info</Button>
+                                :
+                                null
+                            }
+                                                        {
+                                lander.ctdheads.length !== 0
+                                ?
+                                <Button 
+                                    id="LanderButton"
+                                    onClick={flntuNav}
+                                >FLNTU Info</Button>
+                                :
+                                null
+                            }
+                        </div>
                     </Container>
             }
         </Container>
