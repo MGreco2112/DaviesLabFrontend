49d3370f3fa53899b3797d838a5ec084ff92dbf6
Matthew Greco <mgreco2112@tutanota.com>
2025-03-07 16:55:32 -0500

    removed old chart creation function, updated CSS

--- src/components/Data/DisplayData.js
+++ src/components/Data/DisplayData.js
@@ -52,8 +52,8 @@ const DisplayData = () => {
         let headInfo = <h1>Unable To Load Header</h1>
 
         const formProps = {
-            enabled: true,
-            onSubmit: onFormSubmit,
+            enabled: !chartExists,
+            onSubmit: createChart,
             setOuterArr: setDataNames
         }
 
@@ -74,82 +74,41 @@ const DisplayData = () => {
 
     }
 
-    const createTable = () => {
-        //TODO: Create Menu System to allow elements to be added/removed from Chart dynamically via User Input
-            //move chartData Object to top of page
-            //create Form element to house the menu system
-            //dropdown menu with Object.keys() for any given element within the data[]
-            //once selected, create new Object with props:
-                //label: ${dropdown value},
-                //data: data.map(row => row.${dropdown value})
-                //push new Object into datasets[] of chartData Object
-            //once at least one element has been added to array, render button on page (chartData.data.datasets.length > 0)
-            //STRETCH GOALS:
-                //as each key is added to datasets array, remove that option from the dropdown so no duplicates are allowed
-                //allow user to assign desired colors to chart
-                //allow user to determine type of chart created
-
-        //place main page Container into variable to append elements to it
-        const container = document.getElementById("PageContainer");
-        //create Canvas element to hold new Chart once instantiated
-        const canvas = document.createElement("canvas");
-
-        let chartData = {
-            //type of chart
-                //options: bar, bubble, doughnut, pie, line (current), polarArea, radar, scatter
-            type: 'line',
-            //data obj, contains properties for filling chart
-            data: {
-                //labels the bottom of chart
-                labels: data.map(row => row.date),
-                //array of objects for populating the chart
-                datasets:  [
-                    {
-                        //names the data
-                        label: "Temp Degrees (C)",
-                        //pulls the data into chart
-                        data: data.map(row => row.tempDegC)
-                    }
-                ]
-            }
-        }
-
-        //instantiates new Chart object
-            //arg1: element into which Chart is placed
-            //arg2: Data Object for Chart Definition
-        new Chart(canvas, chartData);
-
-        //set ID of canvas element for CSS purposes
-        canvas.id="SensorChart";
-        //flip boolean flag, used to disable button that renders chart
-        setChartExists(true);
-        //add chart canvas to element on page
-        container.appendChild(canvas);
-    }
-
-    const onFormSubmit = () => {
+    const createChart = () => {
         //place main page Container into variable to append elements to it
         const container = document.getElementById("PageContainer");
         //create Canvas element to hold new Chart once instantiated
         const canvas = document.createElement("canvas");
         
-        let chartData = {
+        //initialize chartData obj to hold configuration for Chart obj
+        const chartData = {
+            //set type to line for line chart
             type: 'line',
+            //data obj to contain data within chart
             data: {
+                //label property for labels on bottom of chart
                 labels: data.map(row => row.date),
+                //datasets array to hold each dataset to be displayed within chart
+                    //this is initialized to empty, as contents are generated via user interaction with Form
                 datasets: []
             }
         }
 
+        //iterate through dataNames arr, holding objects sent back from the Form
         for (let i = 0; i < dataNames.length; i++) {
+            //create temporary Object to format data for datasets arr
             const tempDataSetObj = {
+                //label for dataset name
                 label: dataNames[i].label,
+                //map data in data object by the value of the checkbox selected by the User in the form
                 data: data.map(row => row[dataNames[i].data])
             }
 
+            //push each temp obj into the datasets array of the data object inside the chartData object
             chartData.data.datasets.push(tempDataSetObj);
         }
 
+        //craete new Chart object inside the canvas element using the chartData object to populate Chart data
         new Chart(canvas, chartData);
 
         //set ID of canvas element for CSS purposes
