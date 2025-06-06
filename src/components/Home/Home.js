b74be41ad52ef952d1ff7178c6ee61c57c525683
Matthew Greco <mgreco2112@tutanota.com>
2025-06-06 15:32:16 -0400

    added logo to homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Container from "../common/Container";
 import "./Home.css";
+import Logo from "./Davies Lab Logo.jpg";
 
 const Home = () => {
 
     const formatPage = () => {
         return (
             <Container id="HomeContainer">
-            <h1>Welcome to the Davies Lab</h1>
+                <h1>Welcome to the Davies Lab</h1>
 
-            <p>Navigate to Landers to view data</p>
+                <p>Navigate to Landers to view data</p>
+                <img
+                    id="logo"
+                    src={Logo}
+                />
             </Container>
         )
     }
