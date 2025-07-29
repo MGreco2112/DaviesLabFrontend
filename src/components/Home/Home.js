62200db3ee6de21bcd40dac223ca51e5e7b4ad2a
Matthew Greco <mgreco2112@tutanota.com>
2025-07-29 13:46:31 -0400

    created new branch for cache demo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {apiHostURL} from "../../config";
 import Container from "../common/Container";
 import "./Home.css";
 import Splash from "../common/Splash";
 
 const Home = () => {
+
+    useEffect(() => {
+
+        const _populateCache = async () => {
+            const urls = [
+                `${apiHostURL}/api/processed/ctd/headers`,
+                `${apiHostURL}/api/processed/do/headers`,
+                `${apiHostURL}/api/processed/flntu/headers`,
+                `${apiHostURL}/api/processed/albex_ctd/headers`,
+                `${apiHostURL}/api/processed/adcp/headers`
+            ];
+
+            await caches.open("site-cache").then(async (cache) => {
+                await cache
+                    .addAll(urls)
+                    .then(() => console.log("Data added to cache"))
+                    .catch((error) => console.error("Error adding data to cache:", error))
+            });
+        }
+
+        const _retrieveCache = async () => {
+            await caches.open("site-cache").then(async (cache) => {
+                await cache.match(`${apiHostURL}/api/processed/ctd/headers`).then(async function (response) {
+                    if (response) {
+                        const data = await response.json();
+                        console.log(data);
+                    } else {
+                        console.log("Resource Not Found");
+                    }
+                });
+            });
+        }
+
+        
+
+        _populateCache();
+        // _retrieveCache();
+    }, []);
 
     const formatPage = () => {
         return (
