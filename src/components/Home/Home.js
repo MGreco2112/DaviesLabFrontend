61406ad317a52b3463ad83fe1a310348a69d3fcc
Matthew Greco <mgreco2112@tutanota.com>
2025-08-01 14:19:45 -0400

    added new logic to bring in up to three latest landers into homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react";
+import {apiHostURL} from "../../config.js";
+import { useNavigate } from "react-router-dom";
+import Lander from "../landers/Lander.js";
+import axios from "axios";
 import Container from "../common/Container";
 import "./Home.css";
 import Splash from "../common/Splash";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
+
+    const [pageState, setPageState] = useState({
+        loading: true,
+        latestLanders: []
+    });
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const _populateLatestLanders = async () => {
+            try {
+                const res = await axios.get(`${apiHostURL}/api/landers/latest_uploads`);
+
+                setPageState({
+                    ...pageState,
+                    loading: false,
+                    latestLanders: res.data.landers
+                });
+                
+            } catch (err) {
+                console.error(err.message ? err.message : err.response);
+            }
+        }
+
+        if (pageState.loading) {
+            _populateLatestLanders();
+        }
+    }, []);
+
+    const onLanderClick = (id) => {
+        navigate(`/landers/${id}`);
+    }
+
+    const createLanders = () => {
+        
+        return pageState.latestLanders.map(land => {
+            return <Lander className="LanderCard" lander={land} key={land.asdblanderID} onSelect={onLanderClick}/>
+        });
+    }
 
     const formatPage = () => {
         return (
@@ -12,6 +56,18 @@ const Home = () => {
                 <h1>Welcome to the Davies Lab<br/>Lander Timeseries Database</h1>
 
                 <p>Navigate to Landers to view data</p>
+                {
+                    pageState.loading
+                    ?
+                    null
+                    :
+                    <Container className="LandersContainer">
+                        <h2>Latest Uploaded Landers</h2>
+                        <Container className="LandersContainer" id="HomeLanders">
+                            {createLanders()}
+                        </Container>
+                    </Container>
+                }
                 <Splash id="LanderSplash"/>
             </Container>
         )
