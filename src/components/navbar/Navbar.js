0038364edee82a77df3dd27cd371af4875ccf791
Matthew Greco <mgreco2112@tutanota.com>
2025-08-14 14:39:31 -0400

    updated navbar to be thicker

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,47 +8,47 @@ const Navbar = () => {
 
     return (
         <Fragment>
-        <div 
-            style={{
-                position: "fixed",
-                width: "100%",
-                zIndex: 9999,
-                top: 0,
-                left: 0,
-                height: "75px",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-            }}
-            id="Navbar"
-        >
-            <NavLink
-                to="/"
-                id="NavHeader"
+            <div 
+                style={{
+                    position: "fixed",
+                    width: "100%",
+                    zIndex: 9999,
+                    top: 0,
+                    left: 0,
+                    height: "90px",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}
+                id="Navbar"
             >
-                <span className="LogoSpan">
-                    <img
-                        id="LogoImage"
-                        src={Logo}
-                        alt="DODAT Logo"
-                    />
-                </span>
-            </NavLink>
-            <div style={{
-                margin: "0 20px",
-                flexDirection: "row",
-                background: "transparent",
-                userSelect: "none",
-                alignItems: "center",
-            }}>
-                <NavButton to="/" label = "Home"/>
-                <NavButton to="/landers" label = "Landers"/>
-                <NavButton to="/dashboard" label = "Dashboard"/>
-                <NavButton to="/uploads" label="Upload Files"/>
-                <NavButton to="/forDevs" label="API Docs"/>
+                <NavLink
+                    to="/"
+                    id="NavHeader"
+                >
+                    <span className="LogoSpan">
+                        <img
+                            id="LogoImage"
+                            src={Logo}
+                            alt="DODAT Logo"
+                        />
+                    </span>
+                </NavLink>
+                <div style={{
+                    margin: "0 20px",
+                    flexDirection: "row",
+                    background: "transparent",
+                    userSelect: "none",
+                    alignItems: "center",
+                }}>
+                    <NavButton to="/" label = "Home"/>
+                    <NavButton to="/landers" label = "Landers"/>
+                    <NavButton to="/dashboard" label = "Dashboard"/>
+                    <NavButton to="/uploads" label="Upload Files"/>
+                    <NavButton to="/forDevs" label="API Docs"/>
+                </div>
             </div>
-        </div>
-        <div style={{height: "75px"}}/>
+            <div style={{height: "100px"}}/>
         </Fragment>
     )
 }
